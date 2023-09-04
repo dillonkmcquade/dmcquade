56d0580551552b66f01dc4ef197cdb02c3de2f3d
Dillon McQuade <dillonkmcquade@gmail.com>
2023-09-04 12:35:54 -0400

    bug

--- src/data/arrays.js
+++ src/data/arrays.js
@@ -1,3 +1,33 @@
+import { AiFillMail } from "react-icons/ai";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+const LinkedIn = styled(BsLinkedin)`
+  scale: 2;
+  margin: 0 1em;
+  cursor: pointer;
+  color: black;
+  &:hover {
+    color: var(--nord10);
+  }
+`;
+const GitHub = styled(BsGithub)`
+  scale: 2;
+  cursor: pointer;
+  margin: 0 1em;
+  color: black;
+  &:hover {
+    color: var(--nord1);
+  }
+`;
+
+const Mail = styled(AiFillMail)`
+  scale: 2;
+  cursor: pointer;
+  margin: 0 1em;
+  color: black;
+  &:hover {
+    color: var(--nord15);
+  }
+`;
 export const sideBarIcons = [
   {
     href: "https://linkedin.com/in/dillonkmcquade",
