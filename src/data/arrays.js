12ebf10e646b166f1cb091086a76638e5cb06708
Dillon McQuade <dillonkmcquade@gmail.com>
2023-09-04 12:33:26 -0400

    footer icons

---
+++ src/data/arrays.js
@@ -0,0 +1,17 @@
+export const sideBarIcons = [
+  {
+    href: "https://linkedin.com/in/dillonkmcquade",
+    label: "Link to my linkedin",
+    icon: LinkedIn,
+  },
+  {
+    href: "https://github.com/dillonkmcquade",
+    label: "Link to my github",
+    icon: GitHub,
+  },
+  {
+    href: "mailto:dillonkmcquade@gmail.com",
+    label: "Email me",
+    icon: Mail,
+  },
+];
